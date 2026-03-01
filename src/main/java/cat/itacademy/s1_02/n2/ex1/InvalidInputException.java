906468b9e3e69abb3b945fe6ee9770273454dd35
BORGES <jessborgesb@gmail.com>
2026-03-01 14:59:14 +0100

    feat: add InvalidInputException for keyboard input validation

---
+++ src/main/java/cat/itacademy/s1_02/n2/ex1/InvalidInputException.java
@@ -0,0 +1,7 @@
+package cat.itacademy.s1_02.n2.ex1;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
